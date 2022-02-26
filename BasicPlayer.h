8f87fe2328a6213b290baffb58c43497d4f1e35f
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2022-02-26 02:01:20 -0600

    basic testing sucess

--- BasicPlayer.h
+++ BasicPlayer.h
@@ -5,14 +5,24 @@ using namespace std;
 class Basic : public Player{
     public:
         int isGameOver(){
-
-            
+            if (currentLocation->info.getID() == 1){
+                return 2;
+            }
+            else if(currentLocation->info.getGoal() == 1){
+                return 1;
+            }
+            else if(currentLocation->info.getID() == 0 && currentLocation->info.getGoal() == 0){
+                return 0;
+            }
+            else{
+                return 0;
+            }
         };
         void resetPlayerStats(){
             // No Use
         };
         void reportStats(){
-            // No Use
+            cout<<"No player statistics to report."<<endl;
         };
         void consume(Map* mapptr){};
         void use(Map* mapptr){};
