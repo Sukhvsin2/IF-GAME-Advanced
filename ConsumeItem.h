a158d9646070ea5f47979fe77ebdc186dc31740e
Sukhvinder Singh <sukhvindersingh@Sukhvinders-MacBook-Air.local>
2022-02-26 22:45:44 -0600

    Step1 started

---
+++ ConsumeItem.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Item.h"
+
+using namespace std;
+
+class ConsumeItem : public Item{
+
+};
---
+++ UseItem.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Item.h"
+
+using namespace std;
+
+class UseItem : public Item{
+    
+};
