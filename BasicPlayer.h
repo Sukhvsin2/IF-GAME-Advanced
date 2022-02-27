721e9f7a32c82dc20b55a44e3b24691262d8d391
gdodd1 <garrettdodd03@gmail.com>
2022-02-26 20:17:13 -0600

    added and modified some files

--- BasicPlayer.h
+++ BasicPlayer.h
@@ -5,7 +5,7 @@ using namespace std;
 class Basic : public Player{
     public:
         int isGameOver(){
-            
+            return ((currentLocation->info.getID() == true || currentLocation->info.getGoal()== true) ? 1 : 0);
         };
         void resetPlayerStats(){
             // No Use
